78b71d9e5e2c07fb40f0385fb7e09b074e997b73
Sanya Sarieva <s.g.sevdeva@gmail.com>
2024-01-13 11:11:06 +0200

    Fixed compilation issues in db creation script

--- Database/create-script.sql
+++ Database/create-script.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`address` (
   `zip_code` CHAR(45) NULL,
   `country_name` VARCHAR(45) NULL,
   PRIMARY KEY (`adress_id`),
-  UNIQUE INDEX `town_id_UNIQUE` (`adress_id` ASC) VISIBLE)
+  UNIQUE INDEX `town_id_UNIQUE` (`adress_id` ASC) )
 ENGINE = InnoDB;
 
 
@@ -38,11 +38,11 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`privacy` (
   `can_non_friends_comment` TINYINT NOT NULL,
   `can_display_reviews` TINYINT NOT NULL,
   `private_messages` TINYINT NOT NULL,
-  `is_email_visible` TINYINT NOT NULL,
+  `is_email_` TINYINT NOT NULL,
   `allow_search_by_email` TINYINT NOT NULL,
   `challenge_question` TINYTEXT NULL,
   `prompt_to_recommend_books` TINYINT NOT NULL,
-  UNIQUE INDEX `user_id_UNIQUE` (`privacy_id` ASC) VISIBLE,
+  UNIQUE INDEX `user_id_UNIQUE` (`privacy_id` ASC) ,
   PRIMARY KEY (`privacy_id`))
 ENGINE = InnoDB;
 
@@ -73,12 +73,12 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`users` (
   `adress_id` INT UNSIGNED NOT NULL,
   `privacy_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`user_id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE,
-  INDEX `town_id_idx` (`adress_id` ASC) VISIBLE,
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
-  INDEX `privacy_id_fk_idx` (`privacy_id` ASC) VISIBLE,
-  UNIQUE INDEX `privacy_id_UNIQUE` (`privacy_id` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) ,
+  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) ,
+  INDEX `town_id_idx` (`adress_id` ASC) ,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) ,
+  INDEX `privacy_id_fk_idx` (`privacy_id` ASC) ,
+  UNIQUE INDEX `privacy_id_UNIQUE` (`privacy_id` ASC) ,
   CONSTRAINT `u_adress_id_fk_cnstr`
     FOREIGN KEY (`adress_id`)
     REFERENCES `GoodReadsDB`.`address` (`adress_id`)
@@ -98,8 +98,8 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`users_have_friends` (
   `user_id` INT UNSIGNED NOT NULL,
   `friend_id` INT UNSIGNED NOT NULL,
-  INDEX `user_id_idx1` (`user_id` ASC) VISIBLE,
-  INDEX `friend_id_idx` (`friend_id` ASC) VISIBLE,
+  INDEX `user_id_idx1` (`user_id` ASC) ,
+  INDEX `friend_id_idx` (`friend_id` ASC) ,
   PRIMARY KEY (`user_id`, `friend_id`),
   CONSTRAINT `uhf_user_id_cnstr`
     FOREIGN KEY (`user_id`)
@@ -121,8 +121,8 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`genres` (
   `genre_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `genre_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`genre_id`),
-  UNIQUE INDEX `genre_id_UNIQUE` (`genre_id` ASC) VISIBLE,
-  UNIQUE INDEX `genre_name_UNIQUE` (`genre_name` ASC) VISIBLE)
+  UNIQUE INDEX `genre_id_UNIQUE` (`genre_id` ASC) ,
+  UNIQUE INDEX `genre_name_UNIQUE` (`genre_name` ASC) )
 ENGINE = InnoDB;
 
 
@@ -133,8 +133,8 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`languages` (
   `language_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `language` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`language_id`),
-  UNIQUE INDEX `language_id_UNIQUE` (`language_id` ASC) VISIBLE,
-  UNIQUE INDEX `languag_UNIQUE` (`language` ASC) VISIBLE)
+  UNIQUE INDEX `language_id_UNIQUE` (`language_id` ASC) ,
+  UNIQUE INDEX `languag_UNIQUE` (`language` ASC) )
 ENGINE = InnoDB;
 
 
@@ -154,10 +154,10 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`books` (
   `publisher` TEXT(100) NOT NULL,
   `cover_url` VARCHAR(150) NOT NULL,
   PRIMARY KEY (`book_id`),
-  UNIQUE INDEX `book_id_UNIQUE` (`book_id` ASC) VISIBLE,
-  UNIQUE INDEX `ISBN_UNIQUE` (`ISBN` ASC) VISIBLE,
-  INDEX `genre_id_idx` (`genre_id` ASC) VISIBLE,
-  INDEX `language_id_idx` (`language_id` ASC) VISIBLE,
+  UNIQUE INDEX `book_id_UNIQUE` (`book_id` ASC) ,
+  UNIQUE INDEX `ISBN_UNIQUE` (`ISBN` ASC) ,
+  INDEX `genre_id_idx` (`genre_id` ASC) ,
+  INDEX `language_id_idx` (`language_id` ASC) ,
   CONSTRAINT `b_genre_id_cnstr`
     FOREIGN KEY (`genre_id`)
     REFERENCES `GoodReadsDB`.`genres` (`genre_id`)
@@ -178,7 +178,7 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`authors` (
   `author_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `author_name` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`author_id`),
-  UNIQUE INDEX `author_id_UNIQUE` (`author_id` ASC) VISIBLE)
+  UNIQUE INDEX `author_id_UNIQUE` (`author_id` ASC) )
 ENGINE = InnoDB;
 
 
@@ -188,8 +188,8 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`books_have_authors` (
   `book_id` INT UNSIGNED NOT NULL,
   `author_id` INT UNSIGNED NOT NULL,
-  INDEX `book_id_idx` (`book_id` ASC) VISIBLE,
-  INDEX `author_id_idx` (`author_id` ASC) VISIBLE,
+  INDEX `book_id_idx` (`book_id` ASC) ,
+  INDEX `author_id_idx` (`author_id` ASC) ,
   PRIMARY KEY (`book_id`, `author_id`),
   CONSTRAINT `bha_book_id_cnstr`
     FOREIGN KEY (`book_id`)
@@ -210,8 +210,8 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`books_have_editions` (
   `book_id` INT UNSIGNED NOT NULL,
   `edition_id` INT UNSIGNED NOT NULL,
-  INDEX `book_id_idx` (`book_id` ASC) VISIBLE,
-  INDEX `edition_id_idx` (`edition_id` ASC) VISIBLE,
+  INDEX `book_id_idx` (`book_id` ASC) ,
+  INDEX `edition_id_idx` (`edition_id` ASC) ,
   PRIMARY KEY (`book_id`, `edition_id`),
   CONSTRAINT `bhe_book_id_cnstr`
     FOREIGN KEY (`book_id`)
@@ -236,9 +236,9 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`reviews` (
   `book_id` INT UNSIGNED NOT NULL,
   `review_date` DATE NOT NULL,
   PRIMARY KEY (`review_id`),
-  UNIQUE INDEX `review_id_UNIQUE` (`review_id` ASC) VISIBLE,
-  INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
-  INDEX `book_id_idx` (`book_id` ASC) VISIBLE,
+  UNIQUE INDEX `review_id_UNIQUE` (`review_id` ASC) ,
+  INDEX `user_id_idx` (`user_id` ASC) ,
+  INDEX `book_id_idx` (`book_id` ASC) ,
   CONSTRAINT `r_user_id_cnstr`
     FOREIGN KEY (`user_id`)
     REFERENCES `GoodReadsDB`.`users` (`user_id`)
@@ -261,9 +261,9 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`ratings` (
   `book_id` INT UNSIGNED NOT NULL,
   `user_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`rating_id`),
-  UNIQUE INDEX `rating_id_UNIQUE` (`rating_id` ASC) VISIBLE,
-  INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
-  INDEX `book_id_idx` (`book_id` ASC) VISIBLE,
+  UNIQUE INDEX `rating_id_UNIQUE` (`rating_id` ASC) ,
+  INDEX `user_id_idx` (`user_id` ASC) ,
+  INDEX `book_id_idx` (`book_id` ASC) ,
   CONSTRAINT `rat_user_id_cnstr`
     FOREIGN KEY (`user_id`)
     REFERENCES `GoodReadsDB`.`users` (`user_id`)
@@ -294,9 +294,9 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`users_have_books` (
   `user_id` INT UNSIGNED NOT NULL,
   `book_id` INT UNSIGNED NOT NULL,
   `bookshelf_id` INT UNSIGNED NOT NULL,
-  INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
-  INDEX `book_id_idx` (`book_id` ASC) VISIBLE,
-  INDEX `bookshelf_id_idx` (`bookshelf_id` ASC) VISIBLE,
+  INDEX `user_id_idx` (`user_id` ASC) ,
+  INDEX `book_id_idx` (`book_id` ASC) ,
+  INDEX `bookshelf_id_idx` (`bookshelf_id` ASC) ,
   PRIMARY KEY (`user_id`, `book_id`),
   CONSTRAINT `uhb_user_id_cnstr`
     FOREIGN KEY (`user_id`)
@@ -327,8 +327,8 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`messages` (
   `is_read` TINYINT NOT NULL,
   `receiver_folder` CHAR(5) NOT NULL,
   PRIMARY KEY (`message_id`),
-  INDEX `sender_id_idx` (`sender_id` ASC) VISIBLE,
-  INDEX `receiver_id_idx` (`receiver_id` ASC) VISIBLE,
+  INDEX `sender_id_idx` (`sender_id` ASC) ,
+  INDEX `receiver_id_idx` (`receiver_id` ASC) ,
   CONSTRAINT `m_sender_id_cnstr`
     FOREIGN KEY (`sender_id`)
     REFERENCES `GoodReadsDB`.`users` (`user_id`)
@@ -352,8 +352,8 @@ CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`quotes` (
   `user_id` INT UNSIGNED NOT NULL,
   `tags` TEXT(10) NULL,
   PRIMARY KEY (`quote_id`),
-  INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
-  INDEX `author_id_idx` (`author_id` ASC) VISIBLE,
+  INDEX `user_id_idx` (`user_id` ASC) ,
+  INDEX `author_id_idx` (`author_id` ASC) ,
   CONSTRAINT `q_user_id_cnstr`
     FOREIGN KEY (`user_id`)
     REFERENCES `GoodReadsDB`.`users` (`user_id`)
@@ -373,8 +373,8 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`users_like_quotes` (
   `quote_id` INT UNSIGNED NOT NULL,
   `user_id` INT UNSIGNED NOT NULL,
-  INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
-  INDEX `quote_id_idx` (`quote_id` ASC) VISIBLE,
+  INDEX `user_id_idx` (`user_id` ASC) ,
+  INDEX `quote_id_idx` (`quote_id` ASC) ,
   PRIMARY KEY (`user_id`, `quote_id`),
   CONSTRAINT `ulq_user_id_cnstr`
     FOREIGN KEY (`user_id`)
@@ -395,8 +395,8 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`users_like_genres` (
   `user_id` INT UNSIGNED NOT NULL,
   `genre_id` INT UNSIGNED NOT NULL,
-  INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
-  INDEX `genre_id_idx` (`genre_id` ASC) VISIBLE,
+  INDEX `user_id_idx` (`user_id` ASC) ,
+  INDEX `genre_id_idx` (`genre_id` ASC) ,
   PRIMARY KEY (`user_id`, `genre_id`),
   CONSTRAINT `ulg_user_id_cnstr`
     FOREIGN KEY (`user_id`)
@@ -417,7 +417,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `GoodReadsDB`.`reading_challenge` (
   `participant_id` INT UNSIGNED NOT NULL,
   `books_goal` INT UNSIGNED NOT NULL,
-  INDEX `participant_id_idx` (`participant_id` ASC) VISIBLE,
+  INDEX `participant_id_idx` (`participant_id` ASC) ,
   PRIMARY KEY (`participant_id`),
   CONSTRAINT `rch_participant_id_cnstr`
     FOREIGN KEY (`participant_id`)
